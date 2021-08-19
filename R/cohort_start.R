32dcfcfd2c27e83dc9c18efa6c1fb25b1195d261
Travis Gerke <tgerke@mail.harvard.edu>
2021-08-19 09:03:56 -0400

    move print and summary to file with s3 definition

--- R/cohort_start.R
+++ R/cohort_start.R
@@ -22,3 +22,40 @@ cohort_start <- function(.data, label = NULL) {
   )
   x
 }
+
+#' @export
+print.ggconsort_cohort <- function(x, ...) {
+  counts <- cohort_count(x)
+  count_full <- dplyr::filter(counts, .data$cohort == ".full")$count
+  n_cohorts <- nrow(counts) - 1
+
+  desc_obs <- ngettext(
+    count_full,
+    "A ggconsort cohort of %d observation",
+    "A ggconsort cohort of %d observations"
+  )
+  desc_cohorts <- ngettext(n_cohorts, "with %d cohort", "with %d cohorts")
+  description <- sprintf(paste(desc_obs, desc_cohorts), count_full, n_cohorts)
+  description <- paste0(description, if (n_cohorts == 0) ".\n" else ":")
+
+  cat(description)
+  if (n_cohorts < 1) {
+    return()
+  }
+
+  for (i in seq_len(min(n_cohorts, 8)) + 1) {
+    # first cohort is the ".full" cohort
+    cat("\n  - ", counts$cohort[[i]], " (", counts$count[[i]], ")", sep = "")
+  }
+
+  if (n_cohorts > 8) {
+    cat("\n  ...and", n_cohorts - 8, "more.")
+  }
+
+  invisible(x)
+}
+
+#' @export
+summary.ggconsort_cohort <- function(object, ...) {
+  cohort_count(object)
+}
