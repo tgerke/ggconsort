e8e032d8330c94a678a1a3fa2ec6ddce6b7be75c
Travis Gerke <tgerke@mail.harvard.edu>
2021-08-22 00:02:50 -0400

    refactor geoms to accept consort location data

--- R/geom_consort_arrow.R
+++ R/geom_consort_arrow.R
@@ -2,9 +2,10 @@
 
 # sets some reasonable size and style defaults for edges
 
-geom_consort_arrow <- function(x, xend, y, yend, ...) {
+geom_consort_arrow <- function(x, xend, y, yend, data = NULL, ...) {
   geom_segment(
     aes(x = x, xend = xend, y= y, yend = yend),
+    data = data,
     size = 0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(2, "mm"), type = "closed")
   )
