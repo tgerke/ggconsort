9c84ca253d0792077dcfeea2af27e4ecd77191e1
Travis Gerke <tgerke@mail.harvard.edu>
2021-08-22 23:23:00 -0400

    add create_consort_data

--- R/geom_consort_box.R
+++ R/geom_consort_box.R
@@ -6,41 +6,10 @@
 # note that can take vectors like arrow_in = c("top", "left")
 
 geom_consort_box <- function(
-  x, y, label, arrow_in = "none", data = NULL,
+  x, y, label, data = NULL,
   label_color = "black", label_size = "8pt", label_height = 1,
   ...
 ) {
-
-  data <- data %>%
-    mutate(
-      vjust = if_else(
-        arrow_in == "top", 1, .5
-      ),
-      hjust = case_when(
-        arrow_in == "left" ~ 0,
-        arrow_in == "right" ~ 1,
-        TRUE ~ .5
-      )
-    )
-  # if ("top" %in% arrow_in) {
-  #   vjust = 1
-  # } else {
-  #   vjust = .5
-  # }
-  #
-  # if ("left" %in% arrow_in) {
-  #   hjust = 0
-  # } else if ("right" %in% arrow_in) {
-  #   hjust = 1
-  # } else {
-  #   hjust = .5
-  # }
-  #
-  # label <- glue::glue(
-  #   '<span style="color:{label_color}; font-size:{label_size};">
-  #   {label}
-  #   </span>'
-  # )
 
   ggtext::geom_richtext(
     aes(
