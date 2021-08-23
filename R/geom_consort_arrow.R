c1658025ada6a6662de01aa13fd543cdb0253f34
Travis Gerke <tgerke@mail.harvard.edu>
2021-08-23 16:43:38 -0400

    fix line drawing sans arrows

--- R/geom_consort_arrow.R
+++ R/geom_consort_arrow.R
@@ -3,16 +3,17 @@
 # sets some reasonable size and style defaults for edges
 
 geom_consort_arrow <- function(x, xend, y, yend, data = NULL, ...) {
-  geom_segment(
-    aes(x = x, xend = xend, y= y, yend = yend),
-    data = data %>% filter(start != "line"),
-    size = 0.15, linejoin = "mitre", lineend = "butt",
-    arrow = arrow(length = unit(2, "mm"), type = "closed")
+  list(
+    geom_segment(
+      aes(x = x, xend = xend, y= y, yend = yend),
+      data = data %>% filter(start != "line"),
+      size = 0.15, linejoin = "mitre", lineend = "butt",
+      arrow = arrow(length = unit(2, "mm"), type = "closed")
+    ),
+    geom_segment(
+      aes(x = x, xend = xend, y= y, yend = yend),
+      data = data %>% filter(start == "line"),
+      size = 0.15
+    )
   )
-
-  # geom_segment(
-  #   aes(x = x, xend = xend, y= y, yend = yend),
-  #   data = data %>% filter(start == "line"),
-  #   size = 0.15
-  # )
 }
