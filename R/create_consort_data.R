f8a6ed7555fb08e8c1f69b258dd2b175bde6825b
Travis Gerke <tgerke@mail.harvard.edu>
2021-08-25 16:33:09 -0400

    attempt to refactor create_consort_data; broken

--- R/create_consort_data.R
+++ R/create_consort_data.R
@@ -5,48 +5,58 @@
 # consort_boxes and consort_arrows are expected to have certain formats
 # FIXME: to describe and check for those formats
 
-create_consort_data <- function(consort_boxes, consort_arrows) {
-  # first, find out what arrows a box receives for hjust and vjust settings
+create_consort_data <- function(.data, ...) {
   # FIXME: account for 2+ arrows coming in by summarizing those rows
+  # FIXME: need to assert that we've got a cohort with consort data
+
+  consort_boxes <- .data$consort %>%
+    dplyr::filter(type == "box") %>%
+    dplyr::select(name, box_x, box_y, label)
+
+  consort_arrows <- .data$consort %>%
+    dplyr::filter(type %in% c("arrow", "line")) %>%
+    dplyr::select(
+      start, start_side, end, end_side,
+      start_x, start_y, end_x, end_y
+    )
+
   boxes <- dplyr::left_join(
     consort_boxes,
-    consort_arrows %>% dplyr::select(.data$end, .data$end_side),
+    consort_arrows %>% dplyr::select(end, end_side),
     by = c("name" = "end")
   ) %>%
     dplyr::mutate(
       vjust = dplyr::if_else(
-        .data$end_side == "top", 1, .5, missing = .5
+        end_side == "top", 1, .5, missing = .5
       ),
       hjust = dplyr::case_when(
-        .data$end_side == "left" ~ 0,
-        .data$end_side == "right" ~ 1,
+        end_side == "left" ~ 0,
+        end_side == "right" ~ 1,
         TRUE ~ .5
       ),
       type = "box"
     ) %>%
     dplyr::rename(
-      arrow_in = .data$end_side,
-      box_x = .data$x,
-      box_y = .data$y
+      arrow_in = end_side
     )
 
   arrows <- dplyr::left_join(
     consort_arrows,
-    consort_boxes %>% dplyr::select(-.data$label),
+    consort_boxes %>% dplyr::select(-label),
     by = c("start" = "name")
   ) %>%
     dplyr::left_join(
       consort_boxes %>%
-        dplyr::select(-.data$label) %>%
-        dplyr::rename(xend = .data$x, yend = .data$y),
+        dplyr::select(-label) %>%
+        dplyr::rename(xend = x, yend = y),
       by = c("end" = "name")
     ) %>%
     dplyr::mutate(
-      x = dplyr::if_else(is.na(.data$start_x), .data$x, as.numeric(.data$start_x)),
-      y = dplyr::if_else(is.na(.data$start_y), .data$y, as.numeric(.data$start_y)),
-      xend = dplyr::if_else(is.na(.data$end_x), .data$xend, as.numeric(.data$end_x)),
-      yend = dplyr::if_else(is.na(.data$end_y), .data$yend, as.numeric(.data$end_y)),
-      type = dplyr::if_else(.data$start == "line", "line", "arrow")
+      x = dplyr::if_else(is.na(start_x), x, as.numeric(start_x)),
+      y = dplyr::if_else(is.na(start_y), y, as.numeric(start_y)),
+      xend = dplyr::if_else(is.na(end_x), xend, as.numeric(end_x)),
+      yend = dplyr::if_else(is.na(end_y), yend, as.numeric(end_y)),
+      type = dplyr::if_else(start == "line", "line", "arrow")
     ) %>%
     dplyr::select(-dplyr::starts_with("start_"), -dplyr::starts_with("end_"))
 
