24d6c92aef8affa69bd0be15dc0098fd91969a87
jrealgatius <51073451+jrealgatius@users.noreply.github.com>
2022-09-15 15:53:47 +0200

    Update create_consort_data.R
    Add dplyr package in tibble()

--- R/create_consort_data.R
+++ R/create_consort_data.R
@@ -23,7 +23,7 @@ create_consort_data <- function(.data, ...) {
   if (nrow(consort_arrows) == 0) {
     consort_arrows <- bind_rows(
       consort_arrows,
-      tibble(
+      dplyr::tibble(
         start = NA, start_side = NA, end = NA, end_side = NA,
         start_x = NA, start_y = NA, end_x = NA, end_y = NA
       )
@@ -40,7 +40,7 @@ create_consort_data <- function(.data, ...) {
   if (nrow(consort_lines) == 0) {
     consort_lines <- bind_rows(
       consort_lines,
-      tibble(
+      dplyr::tibble(
         start = NA, start_side = NA, end = NA, end_side = NA,
         start_x = NA, start_y = NA, end_x = NA, end_y = NA
       )
@@ -113,7 +113,7 @@ create_consort_data <- function(.data, ...) {
 
   dplyr::full_join(
     boxes,
-    bind_rows(arrows, lines),
+    dplyr::bind_rows(arrows, lines),
     by = "type"
   ) %>%
     dplyr::filter(
