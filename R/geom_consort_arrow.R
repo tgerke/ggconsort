0553292ce9367072e4b0ff081b7c84c1c5f9f937
Travis Gerke <tgerke@mail.harvard.edu>
2026-07-10 18:40:06 -0700

    use linewidth instead of deprecated size aesthetic (#28)

--- R/geom_consort_arrow.R
+++ R/geom_consort_arrow.R
@@ -1,19 +1,17 @@
+#' @rdname geom_consort
 #' @export
-
-# sets some reasonable size and style defaults for edges
-
-geom_consort_arrow <- function(x, xend, y, yend, data = NULL, ...) {
+geom_consort_arrow <- function(...) {
   list(
     ggplot2::geom_segment(
       ggplot2::aes(x = .data$x, xend = .data$xend, y = .data$y, yend = .data$yend),
       data = function(d) dplyr::filter(d, .data$type == "arrow"),
-      size = 0.15, linejoin = "mitre", lineend = "butt",
+      linewidth = 0.15, linejoin = "mitre", lineend = "butt",
       arrow = ggplot2::arrow(length = ggplot2::unit(2, "mm"), type = "closed")
     ),
     ggplot2::geom_segment(
       ggplot2::aes(x = .data$x, xend = .data$xend, y = .data$y, yend = .data$yend),
       data = function(d) dplyr::filter(d, .data$type == "line"),
-      size = 0.15
+      linewidth = 0.15
     )
   )
 }
--- R/geom_consort_line.R
+++ R/geom_consort_line.R
@@ -1,11 +1,10 @@
+#' @rdname geom_consort
+#' @param x,xend,y,yend Coordinates of the line segment drawn by
+#'   `geom_consort_line()`.
 #' @export
-
-# Sometimes lines without arrows are required, and
-# this function allows us to keep styling consistent
-
 geom_consort_line <- function(x, xend, y, yend, ...) {
   ggplot2::geom_segment(
-    ggplot2::aes(x = x, xend = xend, y= y, yend = yend),
-    size = 0.15, linejoin = "mitre", lineend = "butt"
+    ggplot2::aes(x = x, xend = xend, y = y, yend = yend),
+    linewidth = 0.15, linejoin = "mitre", lineend = "butt"
   )
 }
